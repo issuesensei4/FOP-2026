610e9ca2a0c82751e546a362f598d07ac0e56cdf
issuesensei4 <ishaanpatil123@gmail.com>
2026-02-04 18:43:38 +0000

    save

--- class_work/arrays.c
+++ class_work/arrays.c
@@ -2,7 +2,7 @@
 
 /*void main() {
     int a[10];
-    printf("Enter 10 integers:\n");  
+    printf("Enter 10 integers:\n");
     for (int i = 0; i < 10; i++) {
         scanf("%d", &a[i]);
     }
@@ -11,23 +11,28 @@
         printf("%d ", a[i]);    }
 }*/
 
-void main() {
-    int a[3], 
-        b[3], 
+void main()
+{
+    int a[3],
+        b[3],
         c[3];
-    printf("Enter 3 integers for array A:\n");  
-    for (int i = 0; i < 3; i++) {
+    printf("Enter 3 integers for array A:\n");
+    for (int i = 0; i < 3; i++)
+    {
         scanf("%d", &a[i]);
     }
-    printf("Enter 3 integers for array B:\n");  
-    for (int i = 0; i < 3; i++) {
+    printf("Enter 3 integers for array B:\n");
+    for (int i = 0; i < 3; i++)
+    {
         scanf("%d", &b[i]);
     }
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++)
+    {
         c[i] = a[i] + b[i];
     }
     printf("Resultant array C (A + B):\n");
-    for (int i = 0; i < 3; i++) {
-        printf("%d ", c[i]);    
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%d ", c[i]);
     }
-}       
+}
--- class_work/matrix[].c
+++ class_work/matrix[].c
@@ -1,36 +1,45 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int A[2][2], B[2][2], C[2][2];
     int i, j;
 
     // Input first matrix
     printf("Enter elements of Matrix A:\n");
-    for(i = 0; i < 2; i++) {
-        for(j = 0; j < 2; j++) {
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
             scanf("%d", &A[i][j]);
         }
     }
 
     // Input second matrix
     printf("Enter elements of Matrix B:\n");
-    for(i = 0; i < 2; i++) {
-        for(j = 0; j < 2; j++) {
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
             scanf("%d", &B[i][j]);
         }
     }
 
     // Adding matrices
-    for(i = 0; i < 2; i++) {
-        for(j = 0; j < 2; j++) {
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
             C[i][j] = A[i][j] + B[i][j];
         }
     }
 
     // Display result
     printf("Resultant Matrix (Addition):\n");
-    for(i = 0; i < 2; i++) {
-        for(j = 0; j < 2; j++) {
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
             printf("%d ", C[i][j]);
         }
         printf("\n");
